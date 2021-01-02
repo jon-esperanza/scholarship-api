94da16ce61825f74bdc712ad325e5c0c1ecbd3ec
jondinero <jonathanesperanza67@gmail.com>
2021-01-02 17:15:26 -0500

    setup page skeletons with only <HTML>

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,7 +1,29 @@
 export default function Home() {
     return (
         <div>
-          <h2>Home</h2>
+          <h1>Welcome to LearnLatino</h1>
+          <div>
+            <h2>
+                Applying made easier.
+            </h2>
+            <button type="button">Get Started</button>
+          </div>
+          <section>
+            <article>
+              <div>
+                <h3>Use our scholarship database with over $100,000 in awards! </h3>
+                <p>
+                  Using our scholarship API we are able to access an immense database curated by our own developers.
+                </p>
+                <button type="button">Go to database</button>
+              </div>
+              <div>
+                <h3>Join other students and get the resources you need.</h3>
+                <p>Check out our resources page for more information.</p>
+                <button type="button">Go to resources</button>
+              </div>
+            </article>
+          </section>
         </div>
       );
 }
--- client/src/components/scholarships/scholarships.js
+++ client/src/components/scholarships/scholarships.js
@@ -1,7 +1,51 @@
+
 export default function Scholarships() {
     return (
         <div>
           <h2>Scholarships</h2>
+          <div>
+            <label for="button">Want to make a scholarship submission?</label>
+            <button type="button">Click here</button>
+          </div>
+          <div>
+            <form>
+              <input type="search" placeholder="look thru database..."/>
+              <select id="deadlines">
+                <option value="all">All deadlines</option>
+                <option value="1m"> less than 1 months</option>
+                <option value="3m"> less than 3 months</option>
+                <option value="6m"> less than 6 months</option>
+              </select>
+              <select id="awards">
+                <option value="all">All awards</option>
+                <option value="1"> less than $1,000</option>
+                <option value="5"> less than $5,000</option>
+                <option value="10"> less than $10,000</option>
+              </select>
+              <button type="Submit">Search</button>
+            </form>
+          </div>
+          <section>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Deadline</th>
+                  <th>Award</th>
+                  <th>Overview</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>scholarship title</td>
+                  <td>October 23rd</td>
+                  <td>$400</td>
+                  <td>test</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
         </div>
       );
 }
+
